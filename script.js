afed1fce8487cfce8f9f7d6399e7b84c5b3ab2e5
Nursultan Begaliev <begaliev.student@gmail.com>
2021-09-18 16:40:24 +0600

    added a button to quiz

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const quiz = [];
+const quiz = document.querySelector(".quiz");
+
+const button = document.querySelector(".button");
+
+button.addEventListener('click', () => {
+    quiz.className += ' open-quiz';
+    console.log(quiz.className);
+});
